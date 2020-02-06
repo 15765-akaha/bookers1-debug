fcc7f54fe517c5687e74a7b524d02b38d95107ea
15765-akaha <turning2012@gmail.com>
2020-02-06 06:33:22 +0000

    エラーメッセージ確認

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -22,8 +22,9 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-      redirect_to @book, notice: 'Book was successfully created.'
+      redirect_to books_path, notice: 'Book was successfully created.'
     else
+      @books = Book.all
       render :index
     end
   end
